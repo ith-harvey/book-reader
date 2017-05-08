28b92b3b22d9b87da28ea706d2ab831e3194fe2c
Ian Harvey <ith.harvey@gmail.com>
2017-05-08 12:15:42 -0700

    able to show join table for books

--- routes/books.js
+++ routes/books.js
@@ -4,9 +4,19 @@ const db = require('../db')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  db('books').select('*').then( book => {
-    res.render('books', { book, title: 'Books' });
+  console.log('inside of books');
+  return db.select('books.id', 'books.title', 'books.genre', 'books.description', 'authors.first_name', 'authors.last_name', 'authors.id AS authoring_id')
+  .from('books')
+  .innerJoin('books_authors','books.id','books_authors.book_id')
+  .innerJoin('authors','books_authors.author_id','authors.id')
+  .then( books => {
+    console.log('in books');
+    res.render('books', { books, title: 'Books' });
+  }).catch( error => {
+    console.log(error);
+    next(error)
   })
+
 });
 
 module.exports = router;
